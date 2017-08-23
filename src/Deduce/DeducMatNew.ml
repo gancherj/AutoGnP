8cc11079cd8f9de82afd51c34557bdbb873e4a63
Joshua Gancher <jrg358@cornell.edu>
2017-08-23 19:53:09 -0400

    ded

--- src/Deduce/DeducMatNew.ml
+++ src/Deduce/DeducMatNew.ml
@@ -4,18 +4,13 @@ open Util
 
 (* TODO: stack overflows -- bug or i need to move to a heap *)
 
-module Ht = Hashtbl
+module S = Set.Make (
+    struct 
+        type t = (expr * inverter)
+        let compare ei1 ei2 =
+            compare (fst ei1) (fst ei2)
+    end)
 
-let rec subterms e = 
-    match e.e_node with
-    | Nary(_,es') ->
-      List.fold_left Se.union Se.empty (List.map subterms es')
-    | App(_, es') ->
-      List.fold_left Se.union Se.empty (List.map subterms es')
-    | _ ->
-      Se.singleton e
-
-let subterms' es = List.fold_left Se.union Se.empty (List.map subterms es)
 
 let mk_log level = mk_logger "Deduce.DeducMat" level "DeducMat.ml"
 let log_i = mk_log Bolt.Level.DEBUG
@@ -32,124 +27,98 @@ let ei_bop op ei1 ei2 = let (e1, i1) = ei1 in
 
 let matplus_bop e1 e2 = mk_MatPlus [e1;e2]
 
+let setmap f s = S.of_list (List.map f (S.elements s))
 
-let trans_add etbl etbl_copy =
-    Ht.iter (fun e i ->
-        let (e',i') = ei_op mk_MatTrans (e,i) in
-        Ht.add etbl e' i') etbl_copy
+let trans_add eset =
+    eset := S.union !eset (setmap (fun ei -> ei_op mk_MatTrans ei) !eset) 
 
-let splits_add etbl etbl_copy =
-    Ht.iter (fun e i ->
-        if Type.is_Mat_splittable e.e_ty then
-            let (e1,i1) = ei_op mk_MatSplitLeft (e,i) in
-            let (e2, i2) = ei_op mk_MatSplitRight (e,i) in
-            Ht.add etbl e1 i1;
-            Ht.add etbl e2 i2
-        else ()) etbl_copy
-
+let splits_add eset =
+    let lft = (setmap (fun ei ->
+        if Type.is_Mat_splittable (fst ei).e_ty then
+            ei_op mk_MatSplitLeft ei
+        else
+            ei) !eset) in
+    let rght = (setmap (fun ei ->
+        if Type.is_Mat_splittable (fst ei).e_ty then
+            ei_op mk_MatSplitRight ei
+        else
+            ei) !eset) in
+    eset := S.union !eset (S.union lft rght)
     
-let opp_add etbl etbl_copy =
-    Ht.iter (fun e i ->
-        let (e',i') = ei_op mk_MatOpp (e,i) in
-        Ht.add etbl e' i') etbl_copy
-
-        
-let mul_add etbl etbl_copy =
-    Ht.iter (fun e1 i1 ->
-        Ht.iter (fun e2 i2 ->
-            if Type.matmult_compat_ty e1.e_ty e2.e_ty then
-                let (e', i') = ei_bop mk_MatMult (e1,i1) (e2, i2) in
-                Ht.add etbl e' i'
-            else () ) etbl_copy) etbl_copy
-
+let opp_add eset =
+    eset := S.union !eset (setmap (fun ei -> ei_op mk_MatOpp ei) !eset) 
    
-let concat_add etbl etbl_copy =
-    Ht.iter (fun e1 i1 ->
-        Ht.iter (fun e2 i2 ->
+        
+let mul_add eset eset_orig =
+    S.iter (fun (e1, i1) ->
+        S.iter (fun (e2, i2) ->
+            if Type.matmult_compat_ty e1.e_ty e2.e_ty then
+                let ei' = ei_bop mk_MatMult (e1,i1) (e2, i2) in
+                eset := S.add ei' !eset
+            else ()) eset_orig) eset_orig
+
+let concat_add eset eset_orig =
+    S.iter (fun (e1, i1) ->
+        S.iter (fun (e2, i2) ->
             if Type.concat_compat_ty e1.e_ty e2.e_ty then
-                let (e', i') = ei_bop mk_MatConcat (e1,i1) (e2, i2) in
-                Ht.add etbl e' i'
-            else () ) etbl_copy) etbl_copy
+                let ei' = ei_bop mk_MatConcat (e1,i1) (e2, i2) in
+                eset := S.add ei' !eset
+            else ()) eset_orig) eset_orig
 
 
-let plus_add etbl etbl_copy =
-    Ht.iter (fun e1 i1 ->
-        Ht.iter (fun e2 i2 ->
+let plus_add eset eset_orig =
+    S.iter (fun (e1, i1) ->
+        S.iter (fun (e2, i2) ->
             if Type.equal_ty e1.e_ty e2.e_ty then
-                let (e', i') = ei_bop matplus_bop (e1,i1) (e2, i2) in
-                Ht.add etbl e' i'
-            else () ) etbl_copy) etbl_copy
-
-
-let uniq l =
-  let open List in
-  let tbl = Hashtbl.create (length l) in
-  let f l e = 
-    try 
-      let _ = Hashtbl.find tbl e in l
-    with
-    | Not_found -> 
-      Hashtbl.add tbl e ();
-      e::l
-  in
-  List.rev (List.fold_left f [] l)   
+                let ei' = ei_bop matplus_bop (e1,i1) (e2, i2) in
+                eset := S.add ei' !eset
+            else ()) eset_orig) eset_orig
+   
 
 
 
-let extend_iter etbl etbl_orig = 
-    trans_add etbl etbl_orig;
-    opp_add etbl etbl_orig;
-    splits_add etbl etbl_orig;
-    mul_add etbl etbl_orig;
-    concat_add etbl etbl_orig;
-    plus_add etbl etbl_orig;
-    etbl
 
-let norm_etbl etbl =
-    let etbl_new = Ht.create (Ht.length etbl) in
-    Ht.iter (fun e i ->
-        let (e', i') = norm_ei (e,i) in
-        Ht.add etbl_new e' i') etbl;
-    etbl_new
+let extend_iter eset eset_orig = 
+    trans_add eset;
+    opp_add eset;
+    splits_add eset;
+    mul_add eset eset_orig;
+    concat_add eset eset_orig;
+    plus_add eset eset_orig
+    
 
-let try_find e etbl =
-    let etbl_norm = norm_etbl etbl in
+let norm_eset eset =
+    setmap norm_ei eset
+
+let try_find e eset =
+    let eset_norm = norm_eset !eset in
+    let elist = S.elements eset_norm in
     try
-        let i = Ht.find etbl_norm (norm_e e) in
-        print_string "found!!\n";
-        Some (i)
+        let (_,i) = List.find (fun ei -> (fst ei) == (norm_e e)) elist in
+        Some i
     with
     Not_found -> None
 
 
-let rec try_solve etbl etbl_orig e depth =
+let rec try_solve eset eset_orig e depth =
     if depth == 0 then raise Not_found
     else
-        print_string "etbl now has size: ";
-        print_int (Ht.length etbl);
-        print_string "\n ";
-        match try_find e etbl with
+        match try_find e eset with
         | Some i -> i
-        | None -> try_solve (extend_iter etbl etbl_orig) etbl_orig e (depth - 1)
+        | None -> extend_iter eset eset_orig; try_solve eset eset_orig e (depth - 1)
 
-let rec create_etbl so_far ecs = match ecs with
-| [] -> so_far
-| (e,i) :: ecs' -> Ht.add so_far e i; create_etbl so_far ecs'
 
 let solve_mat ecs e =
-    (*log_i (lazy (fsprintf "call solve_mat:"));
+    log_i (lazy (fsprintf "call solve_mat:"));
     List.iter (fun ei ->
         log_i (lazy (fsprintf "(%a, %a)" pp_expr (fst ei) pp_expr
         (expr_of_inverter (snd ei))))) ecs;
-    log_i (lazy (fsprintf "trying to find %a" pp_expr e)); *)
-    (*let ecs = List.filter (fun ei -> not (Se.is_empty (Se.inter (subterms (fst ei))
+    log_i (lazy (fsprintf "trying to find %a" pp_expr e)); 
+    (*let ecs = List.filter (fun ei -> not (S.is_empty (S.inter (subterms (fst ei))
     (subterms e)))) ecs in
     if (List.length ecs == 0) then
         raise Not_found
     else*)
-    print_string "solving with relevant len = ";
-    print_int (List.length ecs);
-    print_string "\n";
-    let etbl = create_etbl (Ht.create 50) ecs in
-    let etbl_orig = Ht.copy etbl in
-    try_solve etbl etbl_orig e 50 
+    let eset = S.of_list ecs in
+    let eset_ref = ref eset in
+    try_solve eset_ref eset e 5 
