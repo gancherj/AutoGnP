9bf1d3a19d41586a08cd929affbd55f046c28aa9
Charlie Jacomme <jacomme@crans.org>
2017-12-07 10:21:32 +0100

    Small corrections on NC_Gasbi

--- src/Deduce/NC_Gasbi.ml
+++ src/Deduce/NC_Gasbi.ml
@@ -6,6 +6,7 @@
     
 open List;;
 open Num;;
+
 (*
 open NormField;;
 open Util;;
@@ -24,12 +25,13 @@ let rec itlist f l b =
     [] -> b
   | (h::t) -> f h (itlist f t b);;
 
-let rec lexord ord l1 l2 =
+let rec lexord_lt ord l1 l2 =
   match (l1,l2) with
-    (h1::t1,h2::t2) -> if ord h1 h2 then true
-                       else h1 = h2 && lexord ord t1 t2
-  | _ -> false;;
-
+    ([],[]) -> false
+   |([],_) -> true
+   |(_,[]) -> false
+   | (h1::t1,h2::t2) -> if ord h1 h2 then true
+                       else h1 = h2 && lexord ord t1 t2;;
 let rec tryfind f l =
   match l with
       [] -> failwith "tryfind"
@@ -41,17 +43,6 @@ let rec distinctpairs l =
    x::t -> itlist (fun y a -> (x,y) :: a) t (distinctpairs t)
   | [] -> [];;
 
-(*
-let gexp e h =
-  if is_FOne h then e
-  else if is_FZ h then mk_GExp (mk_GGen (destr_G_exn e.e_ty)) h
-  else mk_GExp e h;;
-
-let gmult e1 e2 =
-  if is_GOne e1 then e2
-  else if is_GOne e2 then e1
-  else mk_GMult [e1; e2]  ;;
-*)
 (* ------------------------------------------------------------------------- *)
 (* Defining polynomial types                                                 *)
 (* ------------------------------------------------------------------------- *)
@@ -72,17 +63,27 @@ type pol = mon list;;
 (* type pol_i = mon list * Expr.expr; *)
 
 type i_var_set = int list;;
-  
+
+let mk_vmon (i:id_var) (size: id_size*id_size) :mon=
+  {coeff = Num.Int 1; vars = [i]; length = 1; size};; 
+
 (* ------------------------------------------------------------------------- *)
 (* Operations on monomials.                                                  *)
 (* ------------------------------------------------------------------------- *)
 
+let veq_mon (m1:mon) (m2:mon) =
+  (m1.length = m2.length ) && m1.vars=m2.vars;;
+
 let mmul (m1:mon) (m2:mon) :mon  =
+ if snd(m1.size) = fst(m2.size) then
   { coeff = m1.coeff*/m2.coeff;
     vars = m1.vars@m2.vars;
     length = m1.length + m2.length;
     size = (fst(m1.size),snd(m2.size));
-  };;
+  }
+ else failwith "Monoms sizes uncompatible";;
+
+exception NotPrefix;;
 
 let rec is_prefix (m1:id_var list) (m2:id_var list) =
   match (m1,m2) with
@@ -92,7 +93,7 @@ let rec is_prefix (m1:id_var list) (m2:id_var list) =
     |(p1::q1,p2::q2) -> if p1 = p2 then 
                             is_prefix q1 q2
                         else
-                           failwith "Not prefix"
+                           raise NotPrefix;;
 
                              
 
@@ -101,7 +102,7 @@ let rec is_prefix (m1:id_var list) (m2:id_var list) =
 (* ------------------------------------------------------------------------- *)
 
 let morder_lt m1 m2 =
-  m1.length < m2.length || m1.length = m2.length &&  lexord(>) m1.vars m2.vars;;
+  m1.length < m2.length || m1.length = m2.length &&  lexord_lt(<) m1.vars m2.vars;;
 
 (* ------------------------------------------------------------------------- *)
 (* Arithmetic on canonical multivariate polynomials.                         *)
@@ -109,20 +110,21 @@ let morder_lt m1 m2 =
 
 let mpoly_cmul c (pol:pol) :pol =
   if c = Int 0 then []
-  else map (fun {coeff;vars;length;size} -> {coeff = c*/coeff;vars;length;size}) pol;;
+  else map (fun m -> {m with coeff=c*/m.coeff}) pol;;
 
 let mpoly_mmul cm (pol:pol) :pol = map (mmul cm) pol;;
 
-let mpoly_neg (pol) :pol = map (fun {coeff; vars; length; size} -> {coeff=minus_num coeff; vars; length; size}) pol ;;
+let mpoly_neg (pol) :pol = map (fun m -> {m with coeff=minus_num m.coeff}) pol ;;
 
 let rec mpoly_add (l1:pol) (l2:pol):pol =
   match (l1,l2) with
     ([],_) -> l2
   | (_,[]) -> l1
   | (m1::o1,m2::o2) ->
-        if m1.vars = m2.vars then
+        if veq_mon m1 m2 then
           let c = m1.coeff+/m2.coeff and rest = mpoly_add o1 o2 in
-          if c = Num.Int 0 then rest else {coeff=c;vars=m1.vars;length=m1.length;size=m1.size}::rest
+          if c = Num.Int 0 then rest
+          else {m1 with coeff=c}::rest
         else if morder_lt m2 m1 then m1::(mpoly_add o1 l2)
         else m2::(mpoly_add l1 o2);;
 
@@ -138,8 +140,9 @@ let mpoly_sub l1 l2 = mpoly_add l1 (mpoly_neg l2);;
 (* Reduction of a polynom with respect to a base.                            *)
 (* ------------------------------------------------------------------------- *)
 
-(* return all the product K of polys sucht that the leading term of K cancels m *)
-let rec get_all_products (m:id_var list) (polys:pol list) =                  
+(* return all the product K of polys such that the leading term of K is equal m *)
+(* TODO this is wrong ! *)
+let rec get_all_products (m:id_var list) (polys:pol list) : pol list list =                  
     match polys with
       [] -> []
     | (pol)::rpolys ->
@@ -151,7 +154,9 @@ let rec get_all_products (m:id_var list) (polys:pol list) =
              else if v=[] && u=[] then [pol]::(get_all_products m rpolys)
              else let sub_sols = get_all_products v polys in
                   (List.map (fun a-> pol::a) sub_sols)@(get_all_products m rpolys)
-         with Failure _ -> get_all_products m rpolys;;
+         with NotPrefix -> get_all_products m rpolys;;
+
+
 
 (* return all the possible one step reductions of a polynom wrt a base *)
 let reduce_1 (p:pol) (polys:pol list) =
@@ -178,7 +183,14 @@ let rec reduce (p:pol) (polys:pol list)=
   else List.flatten (List.map (fun p -> reduce p polys) reduced_1);;
 
 
+(* ------------------------------------------------------------------------- *)
+(* Computation of critical pairs.                                            *)
+(* ------------------------------------------------------------------------- *)
 
+let rec monom_critical_pairs (m1 :id_var list) (m2:id_var list) : pol*pol list =
+  match is_prefix m1 m2 with
+    ([],[]) -> ([],[])
+  | ;;
 
 (* Exemples *)
 let m1 = {coeff=Num.Int 1; vars=[1]; size=(2,2); length=1};;
